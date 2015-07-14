3504689971be2732b8afa473780d25d09ad1224e
ViMiranda <ViMiranda@192.168.2.54>
2015-07-14 09:26:57 +0200

    teste de commit

--- prueba.php
+++ prueba.php
@@ -1,8 +1,6 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+function saludar(){
+    print 'hola mundo';
+    
+}
